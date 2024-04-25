f649f69a19bab1bd749726d0dc2576a349aa2404
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-04-25 17:17:44 -0400

    fix: edit doc page button

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/merrit/nyrna-website/',
+            'https://github.com/merrit/nyrna-website/edit/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/merrit/nyrna-website/',
+            'https://github.com/merrit/nyrna-website/edit/main',
         },
         theme: {
           customCss: './src/css/custom.css',
